f5c530cf34b9c326c62c91b9761274a82c7b55f8
RyTaus <taus.ryan@gmail.com>
2021-03-24 17:07:01 -0700

    more stuff

--- environment.py
+++ environment.py
@@ -5,7 +5,6 @@ import queue
 import retro
 import gym
 import time
-import simpleaudio as sa
 import pyaudio
 from threading import Thread
 import numpy as np
@@ -13,7 +12,47 @@ import gc
 from gym.envs.classic_control.rendering import SimpleImageViewer
 
 class E(gym.Env):
+
+  def _audio_runner(self, data):
+    audio_stream = self.audio.open(
+      format=pyaudio.paInt8, channels=2, rate=32768, output=True)
+    audio_stream.write(data)
+    audio_stream.stop_stream()
+    audio_stream.close()
+
+
+  def _get_padded_audio(self, frame_count):
+    # Get as much from the buffer as possible.
+    idx = 0
+    out = np.empty([frame_count, 2], dtype=np.uint8)
+    print('======')
+    print(frame_count)
+    print(self.audio_queue.qsize())
+    while True:
+      if idx == frame_count - 1:
+        break
+      try:
+        a = self.audio_queue.get_nowait()
+        # out[idx] = a
+        out[idx] = [200, 200]
+        idx += 1
+      except queue.Empty:
+        break
+    
+    return out
+    # return silence
+
+  def _on_audio_ready(self, in_data, frame_count, time_info, status):
+    # return
+    return self._get_padded_audio(frame_count), pyaudio.paContinue
+
   def __init__(self, rom_path):
+    self.audio = pyaudio.PyAudio()
+    self.audio_queue = queue.Queue()
+    # thread = Thread(target=self._audio_runner)
+    # thread.run()
+
+    # self.audio_stream.start_stream()
 
     self.data = retro.data.GameData()
     self.viewer = SimpleImageViewer()
@@ -34,9 +73,14 @@ class E(gym.Env):
     self.step_number += 1
     # This should return the current RAM
     # return self.em.r
-    self.em.set_button_mask(np.array([1, 0, 1, 1, 0, 0, 0, 0, action], dtype=np.uint8))
+    self.em.set_button_mask(action)
     self.em.step()
     self.data.update_ram()
+    # for a in self.em.get_audio():
+    #   self.audio_queue.put_nowait(a)
+    # Thread(target=self._audio_runner, args=[self.em.get_audio()]).run()
+    # self.audio_queue.put_nowait(self.em.get_audio())
+    # print("added more audio")
     return self._get_ram()
 
   def reset(self):
--- main.py
+++ main.py
@@ -2,19 +2,35 @@ from pprint import pprint
 import time
 import keyboard
 
-import retro
+import numpy as np
+
+# import retro
 
 import environment
+
+def get_input():
+  return np.array([
+    keyboard.is_pressed("o"),
+    False,
+    keyboard.is_pressed("n"),
+    keyboard.is_pressed("m"),
+    keyboard.is_pressed("w"),
+    keyboard.is_pressed("s"),
+    keyboard.is_pressed("a"),
+    keyboard.is_pressed("d"),
+    keyboard.is_pressed("p")
+  ], dtype=np.uint8)
+
 
 e = environment.E("./PokemonBlue.gb")
 print(e.buttons)
 print(dir(e))
 while True:
   time.sleep(1.0 / 59.7275)
-  ram = e.step(keyboard.is_pressed("q"))
-  # print(ram.size)
-  # print(ram[0xFF80 - 0xA000:0xFFB4 - 0xA000])
-  print(ram[0xFF8A - 0xC000])
+  ram = e.step(get_input())
+  # print(ram)
+  print(ram[0xCC26 - 0xA000 + 0x0011])
+  # print(ram[0xFF8A - 0xC000])
   # print(keyboard.is_pressed("q"))
   e.render()
 
